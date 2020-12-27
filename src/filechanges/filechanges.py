0be9005191f6dc416a9f94ac645d363452368771
Garry Offord <drofford@icloud.com>
2020-12-26 20:58:48 -0600

    Now have all unit tests passing

--- src/filechanges/filechanges.py
+++ src/filechanges/filechanges.py
@@ -1,4 +1,5 @@
 import daiquiri
+import inspect
 import logging as LOG
 import os
 import sys
@@ -11,6 +12,25 @@ level = LOG.DEBUG if os.getenv("DEBUG", "") == "Y" else LOG.INFO
 LOG.basicConfig(format="%(asctime)s %(levelname)s : %(message)s", level=level)
 logger = LOG.getLogger()
 
+def debug(msg):
+    f = inspect.currentframe()
+    logger.debug(f"{f.f_back.f_code.co_name}[{f.f_back.f_lineno}]: {msg}")
+
+def info(msg):
+    f = inspect.currentframe()
+    logger.info(f"{f.f_back.f_code.co_name}[{f.f_back.f_lineno}]: {msg}")
+
+
+def error(msg):
+    f = inspect.currentframe()
+    logger.error(f"{f.f_back.f_code.co_name}[{f.f_back.f_lineno}]: {msg}")
+
+
+def fatal(msg):
+    f = inspect.currentframe()
+    logger.error(f"{f.f_back.f_code.co_name}[{f.f_back.f_lineno}]: {msg}")
+    exit(1)
+
 
 def getbasefile():
     """Returns the name of the SQLite DB file"""
@@ -21,12 +41,12 @@ def connectdb() -> sqlite3.Connection:
     """Connects to the SQLite DB"""
     try:
         dbfile = getbasefile() + ".db"
-        logger.debug('connectdb: DB file name is "{}"'.format(dbfile))
+        debug('DB file name is "{}"'.format(dbfile))
         conn = sqlite3.connect(dbfile, timeout=2)
-        logger.debug("connectdb: connected w/ conn = {}".format(conn))
+        debug("connected w/ conn = {}".format(conn))
         return conn
     except Exception as ex:
-        logger.error("connectdb: Error: exception {} caught".format(ex))
+        error("exception {} caught".format(ex))
         sys.stderr.flush()
         exit(1)
 
@@ -37,80 +57,109 @@ def tableexists(table: str) -> bool:
     try:
         conn = connectdb()
         if conn is not None:
-            logger.debug(
-                "tableexists: Connected to database with connection = {}".format(conn)
+            debug(
+                "Connected to database with connection = {}".format(conn)
             )
 
-            query = "SELECT count(id) from {}".format(table)  # Finish this query ...
-            args = (table,)
+            query = f"SELECT id from {table}"  # Finish this query ...
+            debug(f"{query=}")
 
-            result = corecursor(conn, query)  # , args)
-            logger.debug("result = {}".format(result))
+            result = corecursor(conn, query)
+            debug(f"{result=}")
 
-            return False
+            return result
 
     except Exception as ex:
-        logger.error('tableexists: exception of type "{}" caught'.format(type(ex)))
-        logger.error('tableexists: exception "{}" caught'.format(ex))
-        sys.stderr.flush()
+        error('exception "{}" caught'.format(ex))
         return False
 
 
 def corecursor(conn: sqlite3.Connection, query: str, args: list = None) -> bool:
     try:
+        debug(f"A: {conn=}, {query=}")
         cursor = conn.cursor()
-        result = cursor.execute(query, args)
-        logger.debug("corecursor: result = {}".format(result))
+        debug(f"B: {cursor=}")
+        if args is None:
+            result = cursor.execute(query)
+        else:
+            result = cursor.execute(query, args)
+        debug(f"C")
+        debug(f"result = {result}, returning True")
         return True
     except sqlite3.OperationalError as ex:
-        logger.error('corecursor[1]: exception of type "{}" caught'.format(type(ex)))
-        logger.error('corecursor[1]: Error: exception "{}" caught'.format(ex))
+        debug(
+            f'caught exception of type {type(ex)}: "{ex}", returning False'
+        )
         return False
     except Exception as ex:
-        logger.error('corecursor[2]: exception of type "{}" caught'.format(type(ex)))
-        logger.error('corecursor[2]: Error: exception "{}" caught'.format(ex))
-        sys.stderr.flush()
-        exit(1)
+        error(
+            f'caught exception of type {type(ex)}: "{ex}", returning None'
+        )
+        # exit(1)
+        return None
 
 
 def createhashtable(table: str = "files") -> bool:
     result = False
-    query = (
-        "CREATE TABLE {} if not exist (id integer primary key, file_name text)".format(
-            table
-        )
-    )
     try:
+        debug("AAA.1")
         conn = connectdb()
+        debug("AAA.2")
         if conn is not None:
+            debug("BBB.1")
             if tableexists(table):
-                logger.debug("createhashtable: table {} does exist".format(table))
+                debug("BBB.1.A")
+                debug("table {} does exist".format(table))
             else:
-                logger.debug(
-                    "createhashtable: table {} does not exist (yet)".format(table)
+                debug("BBB.2.A")
+                debug("table {} does not exist (yet)".format(table)
                 )
+                QUERY = (
+                    "CREATE TABLE IF NOT EXISTS "
+                    + table
+                    + " (id integer primary key, file_name text)"
+                )
+                debug(f"Query = {QUERY}")
+
+                args = (table,)
                 try:
+                    debug("BBB.2.B")
                     cursor = conn.cursor()
-                    result = cursor.execute(query)
-                    logger.debug("createhashtable: result = {}".format(result))
-                    # To be continued ...
+                    debug("BBB.2.C")
+                    result = cursor.execute(QUERY)
+                    debug("BBB.2.D")
+                    conn.commit()
+                    debug("BBB.2.E")
+                    debug("result = {}".format(result))
+                    result = True
+                    debug("BBB.2.F")
+                except sqlite3.OperationalError as ex:
+                    debug(
+                        'exception of type "{}" caught: {}'.format(
+                            type(ex), ex
+                        )
+                    )
                 except Exception as ex:
-                    logger.error(
-                        'createhashtable[1]: Error: exception "{}" caught'.format(ex)
+                    error(
+                        'exception "{}" caught'.format(ex)
                     )
                     exit(1)
+    except sqlite3.OperationalError as ex:
+        debug(
+            'exception of type "{}" caught: {}'.format(type(ex), ex)
+        )
     except Exception as ex:
-        logger.error('createhashtable[2]: Error: exception "{}" caught'.format(ex))
-        sys.stderr.flush()
+        error('exception "{}" caught'.format(ex))
         exit(1)
+    debug(f"CCC: result = {result}")
     return result
 
 
 def main():
     FILE_TABLE_NAME = "files"
     table_exists = tableexists(FILE_TABLE_NAME)
-    logger.debug(
-        'main: table "{}" does{} exist'.format(
+    debug(
+        'table "{}" does{} exist'.format(
             FILE_TABLE_NAME, "" if table_exists else " not"
         )
     )
--- tests/test_filechanges.py
+++ tests/test_filechanges.py
@@ -3,7 +3,14 @@ import os.path
 import sqlite3
 
 from filechanges import __version__
-from filechanges.filechanges import getbasefile, connectdb
+from filechanges.filechanges import connectdb, createhashtable, getbasefile, tableexists
+
+
+def rm_db_file() -> str:
+    db_filename = getbasefile() + ".db"
+    if os.path.exists(db_filename):
+        os.remove(db_filename)
+    return db_filename
 
 
 def test_version():
@@ -15,22 +22,40 @@ def test_getbasefile():
 
 
 def test_connectdb_new():
-    db_filename = getbasefile() + ".db"
-    if os.path.exists(db_filename):
-        os.remove(db_filename)
+    f = rm_db_file()
+    assert not os.path.exists(f)
 
     conn = connectdb()
     assert conn is not None
     assert isinstance(conn, sqlite3.Connection)
     conn.commit()
     conn.close()
+
+    return f
+
 
 def test_connectdb_extant():
-    test_connectdb_new()
-    assert os.path.exists(getbasefile() + ".db")
+    f = test_connectdb_new()
+    assert os.path.exists(f)
 
     conn = connectdb()
     assert conn is not None
     assert isinstance(conn, sqlite3.Connection)
     conn.commit()
     conn.close()
+
+    # assert False
+
+
+
+def test_tableexists_false():
+    test_connectdb_new()
+
+    assert not tableexists("sparky")
+
+
+def test_createhashtable():
+    f = rm_db_file()
+
+    assert createhashtable("sparky")
+    assert not createhashtable("sparky")
